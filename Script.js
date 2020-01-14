eb640de66e3c53e98a467c0073ecaafdcec85b12
AlejandroGonzalez10 <alejo_841@hotmail.com>
2020-01-13 20:25:38 -0500

    Se agrega ejemplo de select all

--- Script.js
+++ Script.js
@@ -7,13 +7,6 @@ new Vue({
       readonly: false,
       chips: false,
       multiple: false,
-    //   appendIcon: false,
-    //   appendSlot: false,
-    //   appendItemSlot: false,
-    //   prependIcon: false,
-    //   prependSlot: false,
-    //   prependItemSlot: false,
-    //   selectSlot: false,
       model: 'Foo',
     }),
 
@@ -22,32 +15,6 @@ new Vue({
         if (val) this.model = [this.model]
         else this.model = this.model[0] || 'Foo'
       },
-    },
-
-    computed: {
-        likesAllFruit () {
-            return this.selectedFruits.length === this.items.length
-        },
-        likesSomeFruit () {
-            return this.selectedFruits.length > 0 && !this.likesAllFruit
-        },
-        icon () {
-            if (this.likesAllFruit) return 'mdi-close-box'
-            if (this.likesSomeFruit) return 'mdi-minus-box'
-            return 'mdi-checkbox-blank-outline'
-        }
-    },
-
-    methods: {
-        toggle () {
-            this.$nextTick(() => {
-            if (this.likesAllFruit) {
-                this.selectedFruits = []
-            } else {
-                this.selectedFruits = this.items.slice()
-            }
-            })
-        }
     },
 
     vuetify: new Vuetify(),
